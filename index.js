54206cbd32d5073c3d0a8893c78b0a1aab662b64
ifixityemen <42314193+ifixityemen@users.noreply.github.com>
2025-09-06 06:59:36 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is running 🎉');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started on port ' + (process.env.PORT || 3000));
+});
