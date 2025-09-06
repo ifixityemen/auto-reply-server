66d68e745bf1f63a73afe7f232af59591ceb0427
ifixityemen <42314193+ifixityemen@users.noreply.github.com>
2025-09-06 09:58:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Server is running 🎉');
-});
+
+app.get("/rules", (req, res) => {
+  res.json([
+    { keyword: "مرحبا", reply: "أهلًا وسهلًا بك 👋" },
+    { keyword: "شكرا", reply: "العفو 🌹" }
+  ]);
+
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server started on port ' + (process.env.PORT || 3000));
